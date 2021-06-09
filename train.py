1c2b98db29229f6e0a1ca00baa69e23a2333f056
Afsan <khanafsaan11@gmail.com>
2021-06-09 23:35:01 +0530

    added train.py

--- train.py
+++ train.py
@@ -21,3 +21,63 @@ train_datagen = ImageDataGenerator(rescale = 1./255, #All the pixel values would
                                    horizontal_flip = True)
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
+
+
+# preparing the data
+training_set = train_datagen.flow_from_directory(training_path,
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory(testing_path,
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+
+# lets see the data
+training_data = next(training_set)
+label_data = next(test_set) 
+
+
+# define the architecture
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+#input_shape goes reverse if it is theano backend
+#Images are 2D
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+#Most of the time it's (2,2) not loosing many. 
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+#Inputs are the pooled feature maps of the previous layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+#relu - rectifier activation function
+#128 nodes in the hidden layer
+classifier.add(Dense(units = 128, activation = 'relu')) 
+#Sigmoid is used because this is a binary classification. For multiclass softmax
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+#adam is for stochastic gradient descent 
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+classifier.fit(training_set,
+                         epochs = 10,
+                         validation_data = test_set)
+
+
+#Saing the model as a Hierarchical Data Format 5 file 
+classifier.save('model/classifier_81.h5')
